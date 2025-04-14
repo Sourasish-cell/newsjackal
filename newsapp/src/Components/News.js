3ba15b89fc1207882f2a58de636c6d6c9f45c2af
RoyalSN02 <76255696+Sourasish-cell@users.noreply.github.com>
2025-04-14 11:51:33 +0530

    Update News.js

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -7,7 +7,7 @@ import { sampleArticles } from '../data/sampleData'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 // API base URL - points directly to Flask backend
-const API_BASE = 'http://localhost:5000';
+const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export class News extends Component {
   static contextType = ThemeContext;
